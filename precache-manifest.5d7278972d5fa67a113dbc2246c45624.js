5817f63416b664c725f5f2ebdea464bd3d97ac1a
jord2097 <100094374+jord2097@users.noreply.github.com>
2022-03-28 14:39:22 +0100

    Updates

---
+++ precache-manifest.5d7278972d5fa67a113dbc2246c45624.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a208e7d6a7a1fab8201a5b031b938aa9",
+    "url": "/social-app/index.html"
+  },
+  {
+    "revision": "b5de1d389aae37be9376",
+    "url": "/social-app/static/css/2.7c5c0350.chunk.css"
+  },
+  {
+    "revision": "112166f87920d161b871",
+    "url": "/social-app/static/css/main.fb9a2bc5.chunk.css"
+  },
+  {
+    "revision": "b5de1d389aae37be9376",
+    "url": "/social-app/static/js/2.dea09c22.chunk.js"
+  },
+  {
+    "revision": "b2e7ff653f0dd26fbb2e8cb3fe1a2139",
+    "url": "/social-app/static/js/2.dea09c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "112166f87920d161b871",
+    "url": "/social-app/static/js/main.aba913af.chunk.js"
+  },
+  {
+    "revision": "b98e20d0907bea3f81be",
+    "url": "/social-app/static/js/runtime-main.198665ab.js"
+  },
+  {
+    "revision": "d73d3fa4d573c3cf95cfd1d1f8677d98",
+    "url": "/social-app/static/media/hand.d73d3fa4.svg"
+  }
+]);
